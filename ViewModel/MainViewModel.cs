ae4b343401a44b1dff463a7ca58dc08a8c6ef502
tuankiet116 <57905166+tuankiet116@users.noreply.github.com>
2020-12-15 17:55:35 +0700

    Add project files.

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyProject.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        private bool isLoaded = false;
+
+        public MainViewModel()
+        {
+            if (!isLoaded)
+            {
+                this.isLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
