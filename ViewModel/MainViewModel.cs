ce116ef680b4d9c5c4cab05e312cc9b4e20ddc64
doanphilong2k <doanphilong2k@gmail.com>
2020-12-24 01:45:23 +0700

    thêm nhiều thứ

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -11,18 +11,22 @@ namespace MyProject.ViewModel
     public class MainViewModel:BaseViewModel
     {
         public ICommand LoadMainWindow { get; set; }
-        public ICommand GetInWindow { get; set; }
+        public ICommand ImportWindow { get; set; }
+        public ICommand SellWindow { get; set; }
         public ICommand CustomerWindow { get; set; }
         public ICommand ProductWindow { get; set; }
         public ICommand SupplierWindow { get; set; }
+        public ICommand StatisticalWindow { get; set; }
 
         private bool isLoaded = false;
         public MainViewModel()
         {
-            GetInWindow = new RelayCommand<object>((p) => { return true; }, (p) => { GetInWindow wd = new GetInWindow(); wd.ShowDialog(); });
+            ImportWindow = new RelayCommand<object>((p) => { return true; }, (p) => { ImportWindow wd = new ImportWindow(); wd.ShowDialog(); });
+            SellWindow = new RelayCommand<object>((p) => { return true; }, (p) => { SellWindow wd = new SellWindow(); wd.ShowDialog(); });
             CustomerWindow = new RelayCommand<object>((p) => { return true; }, (p) => { CustomerWindow wd = new CustomerWindow(); wd.ShowDialog(); });
             ProductWindow = new RelayCommand<object>((p) => { return true; }, (p) => { ProductWindow wd = new ProductWindow(); wd.ShowDialog(); });
             SupplierWindow = new RelayCommand<object>((p) => { return true; }, (p) => { SupplierWindow wd = new SupplierWindow(); wd.ShowDialog(); });
+            StatisticalWindow = new RelayCommand<object>((p) => { return true; }, (p) => { StatisticalWindow wd = new StatisticalWindow(); wd.ShowDialog(); });
             LoadMainWindow = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 if (!isLoaded)
